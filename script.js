5bdaf61195d920fe3db60fafd3a147998b3a1d61
Thomas Buyens <thomas.buyens@student.ehb.be>
2021-09-27 21:03:50 +0200

    Registration update

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwCeMslNZ4kIo8KRaDPIL
 const sendEmail = 'https://script.google.com/macros/s/AKfycbwwc_EfSX2Ncb0hOfPCBJUsQlhc9OscuYc1BgP70bEnUAscBk131NfnGw/exec'
 const form = document.forms['google-sheet']
 const submit = document.getElementById("submit");
+
+//Small menu
+let menu_click = document.getElementById("menuOpener");
+let menu = document.getElementById("mobile-nav");
+var clicking = false;
+menu_click.addEventListener('click', function () {
+  if (clicking) {
+    menu.classList.remove("clicked");
+    clicking = false;
+  } else {
+    menu.classList.add("clicked");
+    clicking = true;
+  }
+})
+window.addEventListener('touchmove', function () {
+  menu.classList.remove("clicked");
+  clicking = false;
+})
+
 submit.addEventListener('mousedown', function() {
   submit.style.backgroundColor = "#003399";
 })
@@ -83,20 +102,3 @@ function displaySent() {
   $('.message')[0].reset();
 }
 
-//Small menu
-let menu_click = document.getElementById("menuOpener");
-let menu = document.getElementById("mobile-nav");
-var clicking = false;
-menu_click.addEventListener('click', function () {
-  if (clicking) {
-    menu.classList.remove("clicked");
-    clicking = false;
-  } else {
-    menu.classList.add("clicked");
-    clicking = true;
-  }
-})
-window.addEventListener('touchmove', function () {
-  menu.classList.remove("clicked");
-  clicking = false;
-})
